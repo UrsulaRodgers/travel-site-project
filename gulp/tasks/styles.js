181f7654a2d90eee3c1465feaed0838ccfa235df
UrsulaRodgers <ursula.r.rodgers@gmail.com>
2018-02-05 15:15:46 +0000

    CSS updated on landing page

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+	postcss = require('gulp-postcss'),
+	//fs needed for postcss-import to work
+	fs = require('fs'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),
+	cssImport = require('postcss-import'),
+	mixins = require('postcss-mixins');
+
+
+gulp.task('styles', function(){
+	//gulp.src is an asynchronous function, need to return
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+		.on('error', function(errorInfo) {
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+});
